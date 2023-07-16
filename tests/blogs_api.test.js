3cba1815546e8ce8d5fe337a19606d9150fed2bd
Oualid elf <oualid.elferaoui@gmail.com>
2023-07-16 16:53:11 +0100

    moved the post route handler to async/await syntax insrtead of promises and added its integration test

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -2,66 +2,18 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 const Blog = require('../models/blog')
 const app = require('../app')
+const testHelper = require('./helper_test')
 
 const api = supertest(app)
 
 beforeEach(async () => {
-  const initialesBlogs = [
-    {
-      _id: '5a422a851b54a676234d17f7',
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-      __v: 0,
-    },
-    {
-      _id: '5a422b891b54a676234d17fa',
-      title: 'First class tests',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: '5a422ba71b54a676234d17fb',
-      title: 'TDD harms architecture',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-      likes: 0,
-      __v: 0,
-    },
-    {
-      _id: '5a422bc61b54a676234d17fc',
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-      likes: 2,
-      __v: 0,
-    },
-  ]
+  const { initialesBlogs } = testHelper
 
   await Blog.deleteMany({})
   const Blogs = initialesBlogs.map((blog) => new Blog(blog))
   const arrayOfPromises = Blogs.map((blog) => blog.save())
   await Promise.all(arrayOfPromises)
-})
+}, 100000)
 
 test('returns all the blogs posts in the JSON format.', async () => {
   const response = await api.get('/api/blogs')
@@ -74,6 +26,24 @@ test('returns all the blogs posts in the JSON format.', async () => {
 test('every blog post is defined by the id proprety', async () => {
   const response = await api.get('/api/blogs/')
   expect(response.body[0].id).toBeDefined()
+}, 100000)
+
+test('a new blog can be added', async () => {
+  const blog = {
+    title: 'XML is the future',
+    author: 'Bite code!',
+    url: 'https://www.bitecode.dev/p/hype-cycles',
+    likes: 0,
+  }
+
+  const response = await api.post('/api/blogs')
+    .send(blog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = await api.get('/api/blogs')
+  expect((blogs).body).toHaveLength(testHelper.initialesBlogs.length + 1)
+  expect((blogs).body).toContainEqual(response.body)
 }, 100000)
 
 afterAll(async () => {
